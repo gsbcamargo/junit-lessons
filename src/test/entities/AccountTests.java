0ef00d572986febbe49dcc32bdb119e9e795a617
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-03-22 12:23:27 -0300

    created factory pattern and added deposit tests

---
+++ src/test/entities/AccountTests.java
@@ -0,0 +1,40 @@
+package test.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+import tests.factory.AccountFactory;
+
+public class AccountTests {
+	
+	@Test
+	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+		
+		double amount = 200.0;
+		double expectedValue = 196.0;
+		Account account = AccountFactory.createEmptyAccount();
+		
+		account.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, account.getBalance());
+				
+	}
+	
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		
+
+		double expectedValue = 100.0;
+		Account account = AccountFactory.createAccount(expectedValue);
+		double amount = -200.00;
+		
+		account.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, account.getBalance());
+		
+	}
+	
+
+
+}
