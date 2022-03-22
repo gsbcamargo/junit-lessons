983d9f19bb3030b90cd09a606cb901c61bc27901
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-03-22 16:25:46 -0300

    creates fullWithdrawShouldClearBalanceAndReturnWithdrawalValue test case

--- src/test/entities/AccountTests.java
+++ src/test/entities/AccountTests.java
@@ -7,34 +7,44 @@ import entities.Account;
 import tests.factory.AccountFactory;
 
 public class AccountTests {
-	
+
 	@Test
 	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
-		
+
 		double amount = 200.0;
 		double expectedValue = 196.0;
 		Account account = AccountFactory.createEmptyAccount();
-		
+
 		account.deposit(amount);
-		
+
 		Assertions.assertEquals(expectedValue, account.getBalance());
-				
+
 	}
-	
+
 	@Test
 	public void depositShouldDoNothingWhenNegativeAmount() {
-		
 
 		double expectedValue = 100.0;
 		Account account = AccountFactory.createAccount(expectedValue);
 		double amount = -200.00;
-		
-		account.deposit(amount);
-		
-		Assertions.assertEquals(expectedValue, account.getBalance());
-		
-	}
-	
 
+		account.deposit(amount);
+
+		Assertions.assertEquals(expectedValue, account.getBalance());
+
+	}
+
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnWithdrawalValue() {
+
+		double expectedValue = 0.0;
+		double initialBalance = 300.0;
+		Account account = AccountFactory.createAccount(initialBalance);
+
+		double result = account.fullWithdraw();
+
+		Assertions.assertTrue(expectedValue == account.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+	}
 
 }
